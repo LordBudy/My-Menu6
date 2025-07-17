203966f85a066c0f053a2eb1e8d61312b83f1e43
Dmitry Vladimirovich B. <vesna1234510@gmail.com>
2025-07-17 17:01:22 +0300

    новые вводные

--- app/src/main/java/com/example/mymenu/Presentation/Fragments/Account_fragment.kt
+++ app/src/main/java/com/example/mymenu/Presentation/Fragments/Account_fragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.mymenu.R
 
-class Account : Fragment() {
+class Account_fragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
