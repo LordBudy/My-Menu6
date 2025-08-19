70fdc7121e32757743177db9fda73c810f1d0672
budy <vesna1234510@gmail.com>
2025-08-19 18:08:57 +0300

    немножко поковырял дб по миграции

--- app/src/main/java/com/example/mymenu/core/data/db/AppDataBase.kt
+++ app/src/main/java/com/example/mymenu/core/data/db/AppDataBase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.mymenu.core.data.dao.BasketDao
 import com.example.mymenu.core.data.dao.DishCachDao
 import com.example.mymenu.core.data.modelsEntitys.BasketDishEntity
@@ -11,7 +13,7 @@ import com.example.mymenu.core.data.modelsEntitys.DishCachEntity
 
 @Database(
     entities = [BasketDishEntity::class, DishCachEntity::class],
-    version = 1,
+    version = 1,//при каждом изменении меняем версию
     exportSchema = false
 )
 abstract class AppDataBase : RoomDatabase() {
@@ -19,28 +21,41 @@ abstract class AppDataBase : RoomDatabase() {
     abstract fun dishCachDao(): DishCachDao
 
     companion object {
-        // @Volatile - гарантирует, что INSTANCE всегда будет виден всем потокам
         @Volatile
-        // Приватная переменная для хранения единственного экземпляра базы данных (Singleton)
         private var INSTANCE: AppDataBase? = null
-
-        // Метод для получения экземпляра базы данных (Singleton)
+//добавляем новую колонку в таблицу
+        //        val MIGRATION_1_2 = object : Migration(1, 2) {
+//            override fun migrate(database: SupportSQLiteDatabase) {
+//                database.execSQL("ALTER TABLE DishCachEntity ADD COLUMN название новой колонки INTEGER DEFAULT 0 NOT NULL")
+//            }
+//        }
+                              // или добавляем новую таблицу
+//        val MIGRATION_2_3 = object : Migration(2, 3) {
+//            override fun migrate(database: SupportSQLiteDatabase) {
+//                // Создайте новую таблицу
+//                database.execSQL(
+//                    """
+//                    CREATE TABLE cachsCategory (
+//                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+//                        name TEXT NOT NULL
+//                    )
+//                """.trimIndent()
+//                )
+//            }
+//        }
         fun getDatabase(context: Context): AppDataBase {
-            // Если INSTANCE не null, возвращаем его.
-            // Если INSTANCE null, создаем новый экземпляр базы данных в синхронизированном блоке
             return INSTANCE ?: synchronized(this) {
-                // Создаем экземпляр базы данных с помощью Room.databaseBuilder()
                 val instance = Room.databaseBuilder(
-                    // Контекст приложения
                     context.applicationContext,
-                    // Класс базы данных
                     AppDataBase::class.java,
-                    // Название файла базы данных
                     "app_database"
-                ).fallbackToDestructiveMigration(false).build()// Строим базу данных
-                // Сохраняем созданный экземпляр базы данных в INSTANCE
+                )//добавляем новую колонку в таблицу
+                    //.addMigrations(MIGRATION_1_2) // Используем миграцию из того же класса
+                    //или добавляем новую таблицу
+                   // .addMigrations(MIGRATION_2_3)
+                    .fallbackToDestructiveMigration(false)
+                    .build()
                 INSTANCE = instance
-                // Возвращаем созданный экземпляр базы данных.
                 instance
             }
         }
